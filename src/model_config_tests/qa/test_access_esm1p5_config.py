05254d68b7738fe1571e34d28d0ee13020bf9fd2
Tommy Gatti <tommy.gatti@anu.edu.au>
2024-08-06 13:55:28 +1000

    Added  basic ESM1.5 configs test in qa directory

--- src/model_config_tests/qa/test_access_esm1p5_config.py
+++ src/model_config_tests/qa/test_access_esm1p5_config.py
@@ -1,14 +1,14 @@
 # Copyright 2024 ACCESS-NRI and contributors. See the top-level COPYRIGHT file for details.
 # SPDX-License-Identifier: Apache-2.0
 
-"""ACCESS-OM3 specific configuration tests"""
+"""ACCESS-ESM1.5 specific configuration tests"""
 
 import pytest
 
 
-@pytest.mark.access_om3
-class TestAccessOM3:
-    """ACCESS-OM3 Specific configuration and metadata tests"""
+@pytest.mark.access_esm1p5
+class TestAccessEsm1p5:
+    """ACCESS-ESM1.5 Specific configuration and metadata tests"""
 
     def test_pass(self):
         pass
