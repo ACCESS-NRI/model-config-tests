0f1676dcaf9f211ed987a314a9ee7cb396b5774f
Jo Basevi <johanna.basevi@anu.edu.au>
2024-08-27 09:20:09 +1000

    Add a skeleton access-esm1.5 config to test access-esm1p5 marked tests pass

---
+++ tests/resources/access/configurations/README.md
@@ -0,0 +1,4 @@
+# Example test configuration 
+
+This contains some files of configuration from the [ACCESS-NRI/access-esm1.5-configs](https://github.com/ACCESS-NRI/access-esm1.5-configs) repository. The configurations contain only the files
+checked with QA checks, so they are incomplete configurations.
---
+++ tests/test_test_access_esm1p5_config.py
@@ -0,0 +1,33 @@
+import os
+import shlex
+import subprocess
+from pathlib import Path
+
+HERE = os.path.dirname(__file__)
+RESOURCES_DIR = Path(f"{HERE}/resources")
+
+
+def test_test_access_esm1p5_config_release_release_preindustrial():
+    """Test ACCESS-ESM1.5 specific config tests"""
+    access_esm1p5_configs = RESOURCES_DIR / "access" / "configurations"
+    test_config = access_esm1p5_configs / "release-preindustrial+concentrations"
+
+    assert test_config.exists()
+
+    test_cmd = (
+        "model-config-tests -s "
+        # Run all access_esm1p5 specific tests
+        "-m access_esm1p5 "
+        f"--control-path {test_config} "
+        # Use target branch as can't mock get_git_branch function in utils
+        f"--target-branch release-preindustrial+concentrations"
+    )
+
+    result = subprocess.run(shlex.split(test_cmd), capture_output=True, text=True)
+
+    # Expect the tests to have passed
+    if result.returncode:
+        # Print out test logs if there are errors
+        print(f"Test stdout: {result.stdout}\nTest stderr: {result.stderr}")
+
+    assert result.returncode == 0
