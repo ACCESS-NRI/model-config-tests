379089318f536a67c7413bb9f2df846bef5d71fc
anton-seaice <anton.steketee@anu.edu.au>
2025-02-17 16:50:34 +1100

    turn on module use/load check om3 configs

--- src/model_config_tests/qa/test_access_om3_config.py
+++ src/model_config_tests/qa/test_access_om3_config.py
@@ -5,10 +5,19 @@
 
 import pytest
 
+from model_config_tests.qa.test_config import check_manifest_exes_in_spack_location
 
 @pytest.mark.access_om3
 class TestAccessOM3:
     """ACCESS-OM3 Specific configuration and metadata tests"""
 
-    def test_pass(self):
-        pass
+    def test_access_om3_manifest_exe_in_release_spack_location(
+        self, config, control_path
+    ):
+        
+        check_manifest_exes_in_spack_location(
+            model_module_name='access-om3',
+            model_repo_name='ACCESS-OM3',
+            control_path=control_path,
+            config=config,
+        )
