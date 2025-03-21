b15b498fdf7953ae5adeca2bd91ceb3ad2d21cdd
jo-basevi <johanna.basevi@anu.edu.au>
2025-03-21 12:24:15 +1100

    Add Gadi.spack.location download url to QA executable test (#135)
    * Add Gadi.spack.location download url to QA executable test
    * Add test for missing spack.location file for QA test
    * Update tests/qa/test_test_config.py with review suggestions
    
    Co-authored-by: Davide Marchegiani <davide.marchegiani@gmail.com>
    
    ---------
    
    Co-authored-by: Davide Marchegiani <davide.marchegiani@gmail.com>

--- tests/qa/test_test_config.py
+++ tests/qa/test_test_config.py
@@ -1,7 +1,15 @@
 import shlex
 import subprocess
+import warnings
+from unittest.mock import Mock, patch
+
+import pytest
 
 from tests.common import RESOURCES_DIR
+
+# Disable specific warnings from the model_config_tests.qa.test_config module
+warnings.filterwarnings("ignore", category=pytest.PytestUnknownMarkWarning)
+from model_config_tests.qa.test_config import get_spack_location_file
 
 
 def test_test_config_access_om2():
@@ -29,3 +37,53 @@ def test_test_config_access_om2():
         print(f"Test stdout: {result.stdout}\nTest stderr: {result.stderr}")
 
     assert result.returncode == 0
+
+
+@pytest.mark.parametrize(
+    "repo_name, model_version, test_content",
+    [
+        # Release with spack.location file
+        ("ACCESS-OM2", "2024.03.0", "access-om2-2024_03_0"),
+        # Release with Gadi.spack.location file
+        ("ACCESS-OM3", "2025.01.1", "access-om3-2025_01_1"),
+    ],
+)
+def test_get_spack_location_file(repo_name, model_version, test_content):
+    """
+    Test to check that get_spack_location_file runs without error
+    for a couple ACCESS-NRI releases
+    """
+    spack_location = get_spack_location_file(repo_name, model_version)
+    assert test_content in spack_location
+
+
+def test_get_spack_location_file_no_release_artefact():
+    """
+    Test that an error is raised when the release artefact is not found
+    """
+    with pytest.raises(AssertionError, match=r"Failed to find release .*"):
+        get_spack_location_file("fake-repo-name", "fake.module.version")
+
+
+def mock_request_get(url, *args, **kwargs):
+    """Custom side effect function for mocking requests.get to pass the initial
+    initial request (e.g that a release artefact exists)
+    but returns a 404 status for subsequent requests.get calls
+    """
+    response = Mock()
+    if url == "https://github.com/ACCESS-NRI/fake-repo/releases/tag/fake-version":
+        response.status_code = 200
+    else:
+        response.status_code = 404
+    return response
+
+
+def test_get_spack_location_file_no_spack_location():
+    """
+    Test that an error is raised when the spack.location
+    or Gadi.spack.location file is not found in the release artefact
+    """
+    with patch("requests.get", side_effect=mock_request_get):
+        error_msg = r"Failed to download a spack\.location .*"
+        with pytest.raises(AssertionError, match=error_msg):
+            get_spack_location_file("fake-repo", "fake-version")
