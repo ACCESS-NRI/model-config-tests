d2ccda093577a688742cb53ac3cc7f8a3372d6bb
Jo Basevi <johanna.basevi@anu.edu.au>
2025-04-15 12:26:12 +1000

    Add a separate pytest cmd that points to a separate test folder

--- src/model_config_tests/cmds/config_tests_cmd.py
+++ src/model_config_tests/cmds/config_tests_cmd.py
@@ -1,17 +1,21 @@
+"""Tests for a singular model configuration"""
+
 import sys
 from pathlib import Path
 
 # Running pytests using --pyargs does not run pytest_addoption in conftest.py
 # Using workaround as described here:
 # https://stackoverflow.com/questions/41270604/using-command-line-parameters-with-pytest-pyargs
-HERE = Path(__file__).parent
-CONFIG_TESTS = HERE / "config_tests"
+HERE = Path(__file__)
+CONFIG_TESTS_DIR = "config_tests"
 
 
 def main():
     import pytest
 
-    errcode = pytest.main([str(CONFIG_TESTS)] + sys.argv[1:])
+    test_path = str(HERE.parent.parent / CONFIG_TESTS_DIR)
+
+    errcode = pytest.main([test_path] + sys.argv[1:])
     sys.exit(errcode)
 
 
