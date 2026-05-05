7afe6ef06692e05b682b01001745744ed2330011
Qian-HuiChen <QH.ChenOvO@gmail.com>
2026-05-05 11:06:21 +1000

    Uncomment esm1p6 tests; clean up esm1p6/access.out

--- tests/config_tests/qa/test_test_access_esm1p6_config.py
+++ tests/config_tests/qa/test_test_access_esm1p6_config.py
@@ -1,36 +1,31 @@
-# import shlex
-# import shutil
-# import subprocess
+import shlex
+import shutil
+import subprocess
 
-
-# TODO: Add back when we have a sufficient test config released
 def test_test_access_esm1p6_config_release_release_preindustrial(
     tmp_path, isolated_config
 ):
     """Test ACCESS-ESM1.6 specific config tests"""
-    pass
-    # branch_name, config_dir = isolated_config("esm1p6-amip")
-    # # access_esm1p6_configs = RESOURCES_DIR / "access" / "configurations"
-    # # test_config = access_esm1p6_configs / "release-preindustrial+concentrations"
+    branch_name, config_dir = isolated_config("esm1p6-amip")
 
-    # if not config_dir.exists():
-    #     raise FileNotFoundError(f"The test configuration {config_dir} does not exist.")
+    if not config_dir.exists():
+        raise FileNotFoundError(f"The test configuration {config_dir} does not exist.")
 
-    # test_cmd = (
-    #     "model-config-tests -s "
-    #     # Run all access_esm1p6 specific tests
-    #     "-m access_esm1p6 "
-    #     f"--control-path {config_dir} "
-    #     # Use target branch as can't mock get_git_branch function in utils
-    #     f"--target-branch {branch_name}"
-    # )
+    test_cmd = (
+        "model-config-tests -s "
+        # Run all access_esm1p6 specific tests
+        "-m access_esm1p6 "
+        f"--control-path {config_dir} "
+        # Use target branch as can't mock get_git_branch function in utils
+        f"--target-branch {branch_name}"
+    )
 
-    # result = subprocess.run(shlex.split(test_cmd), capture_output=True, text=True)
+    result = subprocess.run(shlex.split(test_cmd), capture_output=True, text=True)
 
-    # # Expect the tests to have passed
-    # if result.returncode:
-    #     # Print out test logs if there are errors
-    #     print(f"Test stdout: {result.stdout}\nTest stderr: {result.stderr}")
+    # Expect the tests to have passed
+    if result.returncode:
+        # Print out test logs if there are errors
+        print(f"Test stdout: {result.stdout}\nTest stderr: {result.stderr}")
 
-    # assert result.returncode == 0
-    # shutil.rmtree(tmp_path)
+    assert result.returncode == 0
+    shutil.rmtree(tmp_path)
