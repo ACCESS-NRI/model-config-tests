02b081527d130ab14a7bc0a4db46a617dfa6bb03
Qianhui Chen <Qianhui.Chen@anu.edu.au>
2026-09-14 16:04:09 +1000

    Use payu status to get job id and status in exp_test_helper.

--- tests/test_exp_test_helper.py
+++ tests/test_exp_test_helper.py
@@ -10,12 +10,7 @@ from netCDF4 import Dataset
 from model_config_tests.exp_test_helper import (
     Experiments,
     ExpTestHelper,
-    parse_exit_status_from_file,
-    parse_gadi_pbs_ids,
-    parse_pbs_submitted_jobs,
-    parse_run_id,
     setup_exp,
-    wait_for_payu_jobs,
 )
 from model_config_tests.models.accessom3 import AccessOm3
 from tests.common import RESOURCES_DIR
@@ -222,121 +217,6 @@ def test_experiment_submit_payu_run_error(mock_run, exp):
     assert f"Return code: {run_return_code}" in str(exec_info.value)
 
 
-@pytest.mark.parametrize(
-    "example_stdout",
-    [
-        "137650670.gadi-pbs\ngadi-pbs ID output is first line\n",
-        "gadi-pbs ID\nIs the last line\n137650670.gadi-pbs\n",
-        "Some output\nJob ID: 137650670.gadi-pbs\nMore output\n",
-    ],
-)
-def test_parse_run_id(example_stdout):
-    run_id = parse_run_id(example_stdout)
-    assert run_id == "137650670.gadi-pbs"
-
-
-@pytest.mark.parametrize(
-    "example_stdout",
-    [
-        "No job ID here\nJust some output\n",
-        "Multiple IDs\n12345.gadi-pbs\n67890.gadi-pbs\n",
-        "Multiple IDs with Job ID prefix\nJob ID: 12345.gadi-pbs\nJob ID: 67890.gadi-pbs\n",
-    ],
-)
-def test_parse_run_id_parsing_error(example_stdout):
-    error_msg = "Expected 1 job ID in payu run submission.*"
-    with pytest.raises(RuntimeError, match=error_msg):
-        parse_run_id(example_stdout)
-
-
-@pytest.mark.parametrize(
-    "stdout_filename, expected_ids",
-    [
-        ("pre-industrial.o137768371", ["137776068.gadi-pbs"]),
-        ("pre-industrial.o137776068", []),
-        ("pre-industrial-hpc.o138768371", ["175421114.gadi-pbs"]),
-    ],
-)
-def test_parse_gadi_pbs_ids(stdout_filename, expected_ids):
-    with open(LOG_DIR / stdout_filename) as f:
-        stdout = f.read()
-    assert parse_gadi_pbs_ids(stdout) == expected_ids
-
-
-@pytest.mark.parametrize(
-    "stdout_filename, expected_job_id",
-    [
-        ("pre-industrial.o137768371", "137776068.gadi-pbs"),
-        ("pre-industrial.o137776068", None),
-    ],
-)
-def test_parse_pbs_submitted_jobs(stdout_filename, expected_job_id):
-    with open(LOG_DIR / stdout_filename) as f:
-        stdout = f.read()
-    run_id = parse_pbs_submitted_jobs(stdout)
-    assert run_id == expected_job_id
-
-
-@pytest.mark.parametrize(
-    "stdout",
-    [
-        ("qsub -q normal ... - path/to/env/bin/python /path/to/env/bin/payu-run\n"),
-        (
-            "some test output\n"
-            "qsub -q normal ... - path/to/env/bin/python /path/to/env/bin/payu-run\n"
-            "exit status: 0"
-        ),
-    ],
-)
-def test_parse_pbs_submitted_jobs_no_job_id_error(stdout):
-    """
-    Test that parse_pbs_submitted_jobs raise an error when there's
-    no job_ids for submitted run jobs in the stdout"""
-    expected_error = "No job ID found in stdout file for subsequent payu run job"
-    with pytest.raises(RuntimeError, match=expected_error):
-        parse_pbs_submitted_jobs(stdout)
-
-
-@pytest.mark.parametrize(
-    "stdout, expected_run_id",
-    [
-        (
-            "123456.gadi-pbs\n"
-            "qsub -q normal ... - path/to/env/bin/python /path/to/env/bin/payu-run\n"
-            "654321.gadi-pbs",
-            "654321.gadi-pbs",
-        ),
-        (
-            "qsub -q normal ... - path/to/env/bin/python /path/to/env/bin/payu-run\n"
-            "123456.gadi-pbs\n654321.gadi-pbs\n67890.gadi-pbs\n",
-            "67890.gadi-pbs",
-        ),
-    ],
-)
-def test_parse_pbs_submitted_jobs_more_job_ids(stdout, expected_run_id):
-    """
-    When there are more job IDs the payu run jobs,
-    raise a warning - maybe the setup/run userscripts submitted a PBS job?
-    """
-    expected_error_msg = r"Found more than 1 job IDs in stdout file .*"
-    with pytest.warns(UserWarning, match=expected_error_msg):
-        run_id = parse_pbs_submitted_jobs(stdout)
-    assert run_id == expected_run_id
-
-
-@pytest.mark.parametrize(
-    "stdout_filename, expected_exit_status",
-    [
-        ("pre-industrial.o137768371", 0),
-        ("pre-industrial.o137776068", 0),
-        ("example_failed_job.o1234", 1),
-    ],
-)
-def test_parse_exit_status_from_file(stdout_filename, expected_exit_status):
-    with open(LOG_DIR / stdout_filename) as f:
-        stdout = f.read()
-    assert parse_exit_status_from_file(stdout) == expected_exit_status
-
 
 def mock_wait_for_qsub(job_id):
     """
@@ -344,74 +224,6 @@ def mock_wait_for_qsub(job_id):
     """
     return None
 
-
-@pytest.mark.parametrize(
-    "job_id, expected_output_filenames",
-    [
-        # Test two sequential payu run cycles
-        (
-            "137768371.gadi-pbs",
-            [
-                "pre-industrial.o137768371",
-                "pre-industrial.e137768371",
-                "pre-industrial.o137776068",
-                "pre-industrial.e137776068",
-            ],
-        ),
-        # Test with job_id of the second payu run
-        (
-            "137776068.gadi-pbs",
-            [
-                "pre-industrial.o137776068",
-                "pre-industrial.e137776068",
-            ],
-        ),
-    ],
-)
-def test_wait_for_payu_jobs(job_id, expected_output_filenames):
-    mock_wait_for_qsub_func = mock_wait_for_qsub
-    control_path = LOG_DIR
-    output_files = wait_for_payu_jobs(control_path, job_id, mock_wait_for_qsub_func)
-    output_filenames = [Path(filepath).name for filepath in output_files]
-    assert output_filenames == expected_output_filenames
-
-
-def test_read_job_output_file_no_stdout_files(tmp_path):
-    """Test error raised when there is no stdout files."""
-    job_id = "1234.gadi-pbs"
-    expected_msg = r"Expected 1 stdout file for job ID 1234, but found 0.*"
-    with pytest.raises(RuntimeError, match=expected_msg):
-        wait_for_payu_jobs(tmp_path, job_id, mock_wait_for_qsub)
-
-
-def test_read_job_output_file_multiple_stdout_files(tmp_path):
-    """Test error raised when there is multiple stdout files."""
-    job_id = "1234.gadi-pbs"
-    # Create multiple stdout files with the same job ID
-    stdout_file_1 = tmp_path / "example_job.o1234"
-    stdout_file_1.write_text("Test stdout file 1", encoding="utf-8")
-    stdout_file_2 = tmp_path / "example_job2.o1234"
-    stdout_file_2.write_text("Test stdout file 2", encoding="utf-8")
-    expected_msg = r"Expected 1 stdout file for job ID 1234, but found 2.*"
-    with pytest.raises(RuntimeError, match=expected_msg):
-        wait_for_payu_jobs(tmp_path, job_id, mock_wait_for_qsub)
-
-
-def test_wait_for_qsub_job_exited_with_error(tmp_path):
-    """
-    Test that the wait_for_qsub function raises an exception when the job exits with an error.
-    """
-    failed_job_id = "1234.gadi-pbs"
-    failed_job_stdout = "example_failed_job.o1234"
-    # Copy failed job stdout file to the tmp directory
-    shutil.copy(LOG_DIR / failed_job_stdout, tmp_path)
-    # Create an stderr file
-    stderr_file = tmp_path / "example_failed_job.e1234"
-    stderr_file.write_text("Test stderr file", encoding="utf-8")
-
-    expected_msg = "Payu run job failed with exit status 1"
-    with pytest.raises(RuntimeError, match=expected_msg):
-        wait_for_payu_jobs(tmp_path, failed_job_id, mock_wait_for_qsub)
 
 
 def test_experiment_wait_for_payu_run(exp, tmp_path):
